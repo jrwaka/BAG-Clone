6c2dc09eda0ac1c6538fddf5ec69bea70a50510d
manishimwe mugisha jean paul <manishimwemugishaj40@gmail.com>
2024-08-02 07:15:05 +0200

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,7 @@
 module.exports = {
   content: ["./src/components/*.{js,ts,jsx,tsx,css}"],
   theme: {
-    extend: {
-      animation: {
-        marquee: "marquee 25s linear infinite",
-        marquee2: "marquee2 25s linear infinite",
-        marquee3: "marquee 25s linear infinite",
-        marquee4: "marquee2 25s linear infinite",
-      },
-
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        marquee2: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-        marquee3: {
-          "100%": { transform: "translateX(0%)" },
-          "0%": { transform: "translateX(-100%)" },
-        },
-        marquee4: {
-          "100%": { transform: "translateX(0%)" },
-          "0%": { transform: "translateX(100%)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
