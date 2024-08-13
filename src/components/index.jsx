2bc509efd703fe0478bd1a5d434d8fe66ff11b19
manishimwe mugisha jean paul <manishimwemugishaj40@gmail.com>
2024-08-13 09:01:49 +0200

    f

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,11 +4,9 @@ import Carsaul from "./carsaul";
 import FirstMainSection from "./FirstMainSection";
 import NavBar from "./NavBar";
 import Talents from "./Talents";
-<<<<<<< HEAD
 
-=======
 import Experience from "./experience";
->>>>>>> 5af8b3e3da3df6b20b3361eb36e004020dc47c6a
+
 const Index = () => {
   return (
     <div className="   pb-[100px] ">
