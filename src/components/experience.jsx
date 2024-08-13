1bb36473f790bd8f153c74aa89056a1666acdf51
jrwaka <j.rwaka@alustudent.com>
2024-08-13 14:10:38 +0200

    New updates

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -4,12 +4,12 @@ function Experience() {
   return (
     <div>
       <div className="flex mt-28 gap-5 px-[60px]">
-        <div className="bg-blue-100 ps-[80px] h-[650px] rounded-3xl flex flex-row flex-1  gap-1">
-          <div className="ml-[-40px] mt-3 w-[650px]  bg-white rounded-3xl">
+        <div className="bg-blue-100 ps-[80px] rounded-3xl flex flex-row flex-1  gap-1">
+          <div className="ml-[-40px] mt-3 w-[600px]  bg-white rounded-3xl justify-evenly flex flex-col">
             {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
 
-            <div className="bg-indigo-300 ">
-              <p className="mt-[5rem] ml-8 font-bold  text-xl flex gap-2">
+            <div className= "px-4 flex gap-3">
+                <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -24,41 +24,20 @@ function Experience() {
                     d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
                   />
                 </svg>
+                </div>
+                <div className="flex flex-col gap-2">
+              <p className="font-bold text-xl flex">
                 Top Job Opportunities
               </p>
-              <p className="ml-[4.8rem] mr-8 font-normal  flex-auto text-slate-400">
+              <p className="font-normal flex-auto text-slate-400 pr-5 bg-white text-sm">
                 Connect with leading employers across Africa for job and
                 internship opportunities.
               </p>
+              </div>
             </div>
-            {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
-            <div className="bg-purple-300">
-              <p className="mt-14 ml-8 font-bold text-xl  flex gap-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-8"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                  />
-                </svg>
-                Career Growth
-              </p>
-              <p className="ml-[4.8rem] mr-8 font-normal text-wrap  text-slate-400">
-                Gain practical experience by working remotely on projects that
-                align with your career aspirations.
-              </p>
-            </div>
-            {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
 
-            <div className="bg-green-300">
-              <p className="mt-14 ml-8 font-bold text-xl  flex gap-3">
+            <div className= "px-4 flex gap-3">
+                <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -70,18 +49,53 @@ function Experience() {
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+                    d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
                   />
                 </svg>
-                Career Guidance
+                </div>
+                <div className="flex flex-col gap-2">
+              <p className="font-bold text-xl flex">
+                Top Job Opportunities
               </p>
-              <p className="ml-[4.8rem] mr-8 font-normal text-wrap  text-slate-400">
-                Benefit from mentorship by seasoned professionals, guiding you
-                to success in your chosen field.
+              <p className="font-normal flex-auto text-slate-400 pr-5 bg-white text-sm">
+                Connect with leading employers across Africa for job and
+                internship opportunities.
               </p>
+              </div>
             </div>
-          </div>
-          <div className="mt-16 ml-20 flex-wrap gap-5">
+
+            <div className= "px-4 flex gap-3">
+                <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-8"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
+                  />
+                </svg>
+                </div>
+                <div className="flex flex-col gap-2">
+              <p className="font-bold text-xl flex">
+                Top Job Opportunities
+              </p>
+              <p className="font-normal flex-auto text-slate-400 pr-5 bg-white text-sm">
+                Connect with leading employers across Africa for job and
+                internship opportunities.
+              </p>
+              </div>
+            </div>
+            </div>
+            
+            {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
+            
+          <div className="py-16 ml-20 flex-wrap gap-5">
             <p className="font-bold text-4xl text-center">
               Popular Work Experiences
             </p>
