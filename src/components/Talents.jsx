48b58472c94ea6f22b0e5bad19dce6c7feaf8fff
manishimwe mugisha jean paul <manishimwemugishaj40@gmail.com>
2024-08-13 09:08:28 +0200

    all changes

--- src/components/Talents.jsx
+++ src/components/Talents.jsx
@@ -26,7 +26,6 @@ function Talents() {
             improvement.
           </p>
         </div>
-<<<<<<< HEAD
         <div className="w-[400px]  py-10">
           <div className="bg-yellow-200 flex flex-col gap-3  items-center">
             <img className="" src="./cup.ee89f4a1.svg" alt="hello" />
@@ -40,21 +39,21 @@ function Talents() {
             </p>
           </div>
           <div></div>
-=======
-        <div className="bg-slate-800 flex flex-col p-10 rounded-3xl items-center ">
-          <img
-            className="mb-4"
-            src="./sponser/building.80c20bd7.svg"
-            alt="home"
-          />
-          <h1 className="text-center font-bold 2xl:text-3xl xl:text-4xl  md:text-xl text-lg text-white">
-            Get Employed
-          </h1>
-          <p className="text-center text-sm   font-normal text-white mt-2">
-            We match you with job and internship opportunities that align with
-            your skills, interests, and performance scores.
-          </p>
->>>>>>> 5af8b3e3da3df6b20b3361eb36e004020dc47c6a
+
+          <div className="bg-slate-800 flex flex-col p-10 rounded-3xl items-center ">
+            <img
+              className="mb-4"
+              src="./sponser/building.80c20bd7.svg"
+              alt="home"
+            />
+            <h1 className="text-center font-bold 2xl:text-3xl xl:text-4xl  md:text-xl text-lg text-white">
+              Get Employed
+            </h1>
+            <p className="text-center text-sm   font-normal text-white mt-2">
+              We match you with job and internship opportunities that align with
+              your skills, interests, and performance scores.
+            </p>
+          </div>
         </div>
       </div>
     </div>
