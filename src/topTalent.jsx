a7555c03f7a149c5525487a8e9bd5377f29972a8
manishimwe mugisha jean paul <manishimwemugishaj40@gmail.com>
2024-08-14 19:17:52 +0200

    today change on import new components [starting and topTalents]

--- src/topTalent.jsx
+++ src/topTalent.jsx
@@ -3,27 +3,18 @@ import React from "react";
 function Toptalented() {
   return (
     <div>
-      <div className="flex mt-28 gap-5 px-[60px] mb-28">
-        <div className="bg-blue-100 ps-[80px] rounded-3xl flex flex-row flex-1  gap-1">
-          <div className="ml-[-40px] mt-3 w-[600px]  bg-white rounded-3xl justify-evenly flex flex-col">
+      <div className="flex mt-28 gap-5 px-[60px]  mb-28">
+        <div className=" bg-blue-100 p-8  justify-center rounded-3xl flex    gap-8">
+          <div className="  w-[450px]  bg-white rounded-3xl justify-evenly hidden md:flex flex-col">
             {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
 
-            <div className="px-4 flex gap-3">
+            <div className="px-4 flex gap-3 ">
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-8"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
-                  />
-                </svg>
+                <img
+                  className="w-16"
+                  src="./icons/arcticons--number-circle-1.svg"
+                  alt=""
+                />
               </div>
               <div className="flex flex-col gap-2">
                 <p className="font-bold text-xl flex">Top Job Opportunities</p>
@@ -36,20 +27,11 @@ function Toptalented() {
 
             <div className="px-4 flex gap-3">
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-8"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
-                  />
-                </svg>
+                <img
+                  className="w-16"
+                  src="./icons/flash-outline.svg"
+                  alt="hello"
+                />
               </div>
               <div className="flex flex-col gap-2">
                 <p className="font-bold text-xl flex">Top Job Opportunities</p>
@@ -62,20 +44,7 @@ function Toptalented() {
 
             <div className="px-4 flex gap-3">
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-8"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
-                  />
-                </svg>
+                <img className="w-16" src="./icons/path.svg" alt="hello" />
               </div>
               <div className="flex flex-col gap-2">
                 <p className="font-bold text-xl flex">Top Job Opportunities</p>
@@ -89,39 +58,42 @@ function Toptalented() {
 
           {/** STYLE THE RESPONSIVENESS ACCORDING TO THIS DIV AND TRY TO AVOID MARGIN AND PADDING USE FLEXING AND GAP */}
 
-          <div className="py-16 ml-20 flex-wrap gap-5">
-            <p className="font-bold text-4xl text-center">
-              Popular Work Experiences
-            </p>
-            <p className="mt-5 text-center">
-              Kickstart your career growth by joining our virtual job
-              simulations. We're excited to support your journey towards
-              achieving your professional goals.
-            </p>
-            <div className="grid grid-cols-3 grid-flow-col  gap-2 mt-5">
-              <div className="bg-red-600 flex flex-col justify-center overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Bonheur</h2>
+          <div className="py-16   flex-wrap gap-5 ">
+            <div className=" ">
+              <p className="font-bold text-4xl text-center">
+                Popular Work Experiences
+              </p>
+              <p className="mt-5 text-center">
+                Kickstart your career growth by joining our virtual job
+                simulations. We're excited to support your journey towards
+                achieving your professional goals.
+              </p>
+            </div>
+
+            <div className="md:grid md:grid-cols-3 overflow-x-scroll md:overflow-x-visible  gap-6 mt-5  pt-6 ">
+              <div className="bg-blue-200 flex flex-col justify-between  overflow-hidden  rounded-3xl xl:w-[14.375rem]  w-[17rem]h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Bonheur</h2>
                 <img src="./persons/bonheur.png" alt="Bonheur" />
               </div>
-              <div className="bg-white flex flex-col justify-center w-32 overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Deng</h2>
+              <div className="bg-white flex flex-col justify-between  overflow-hidden  rounded-3xl  xl:w-[14.375rem]  w-[17rem]h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Bonheur</h2>
                 <img src="./persons/Deng.png" alt="Deng" />
               </div>
-              <div className="bg-white flex flex-col justify-center w-1/3 overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Lwanda</h2>
-                <img src="./persons/Lwanda.webp" alt="Lwanda" />
+              <div className="from-gray-500 bg-gradient-to-b to-white t0-50% flex flex-col justify-between  overflow-hidden  rounded-3xl  xl:w-[14.375rem]  w-[17rem]h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Mutesi</h2>
+                <img src="./persons/Mutesi.webp" alt="Lwanda" />
               </div>
-              <div className="bg-white flex flex-col justify-center w-1/3 overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Mutesi</h2>
-                <img src="./persons/Mutesi.webp" alt="Mutesi" />
+              <div className="from-gray-500 bg-gradient-to-b to-white t0-50% flex flex-col justify-between  overflow-hidden  rounded-3xl  xl:w-[14.375rem]  w-[17rem]h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Bonheur</h2>
+                <img src="./persons/Nkurunziza.webp" alt="Mutesi" />
               </div>
-              <div className="bg-white flex flex-col justify-center w-1/3 overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Nkurunziza</h2>
-                <img src="./persons/Nkurunziza.webp" alt="Nkurunziza" />
+              <div className="bg-blue-200 flex flex-col justify-between  overflow-hidden  rounded-3xl  xl:w-[14.375rem]  w-[17rem]h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Bonheur</h2>
+                <img src="./persons/Yves.webp" alt="Nkurunziza" />
               </div>
-              <div className="bg-white flex flex-col justify-center w-1/3 overflow-hidden  rounded-3xl p-5">
-                <h2 className="font-bold text-lg text-center">Yves</h2>
-                <img src="./persons/Yves.webp" alt="Yves" />
+              <div className="bg-white flex flex-col justify-between  overflow-hidden  rounded-3xl  xl:w-[14.375rem]  w-[17rem] h-[11.125rem]">
+                <h2 className="font-bold text-lg text-center mt-2">Bonheur</h2>
+                <img src="./persons/Lwanda.webp" alt="Yves" />
               </div>
             </div>
           </div>
